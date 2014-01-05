b18c1b326bcb8194265aeb665a6128acb2fb1b5a
Pieux Xi <pieuxxi@gmail.com>
2014-01-05 12:46:43 +0800

    rename the app as helmes

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'hermes' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array or 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'ngRoute', 'ngAnimate', 'starter.services', 'starter.controllers'])
+// 'hermes.services' is found in services.js
+// 'hermes.controllers' is found in controllers.js
+angular.module('hermes', ['ionic', 'ngRoute', 'ngAnimate', 'hermes.services', 'hermes.controllers'])
 
 .config(function ($compileProvider){
   // Needed for routing to work
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('hermes.controllers', [])
 
 .controller('AppCtrl', function($scope) {
   // Main app controller, empty for the example
